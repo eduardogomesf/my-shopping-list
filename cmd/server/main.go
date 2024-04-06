a9ced08cef06dda2d239e38490e858d38ac56396
eduardogomesf <eduardogoomesf@gmail.com>
2024-04-06 19:40:26 -0300

    feat: add route for creating shopping list

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"fmt"
+
 	config "github.com/eduardogomesf/shopping/configs"
+	"github.com/eduardogomesf/shopping/internal/infra/databases/models"
+	"github.com/eduardogomesf/shopping/internal/infra/databases/repositories"
 	webserver "github.com/eduardogomesf/shopping/internal/infra/web"
+	handler "github.com/eduardogomesf/shopping/internal/infra/web/handlers"
+	usecases "github.com/eduardogomesf/shopping/internal/use-cases"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
 	conf := config.LoadConfig(".")
 	ws := webserver.NewWebServer(conf.APPPort)
+
+	connectionUrl := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=UTC"
+	db, err := gorm.Open(postgres.Open(connectionUrl), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println(err)
+		panic("failed to connect to database")
+	}
+
+	db.AutoMigrate(&models.ShoppingListModel{})
+
+	shoppingListRepository := repositories.NewShoppingListRepository(db)
+	addShoppingListUseCase := usecases.AddShoppingListUseCase{
+		GetActiveShoppingListByNameRepository: shoppingListRepository,
+		CreateShoppingListRepository:          shoppingListRepository,
+	}
+	addNewShoppingListHandler := handler.NewAddShoppingListHandler(addShoppingListUseCase)
+	ws.AddHandler("POST", "/shopping-lists", addNewShoppingListHandler.Handle)
+
 	ws.Start()
 }
--- go.mod
+++ go.mod
@@ -7,10 +7,15 @@ require github.com/spf13/viper v1.18.2
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	gorm.io/driver/postgres v1.5.7 // indirect
 )
 
 require (
